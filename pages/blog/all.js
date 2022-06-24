f71683eddb29cdf2dbfdb43459cc1e1a23b10c7c
sreedhar <sreedharks2003@gmail.com>
2022-06-25 01:41:17 +0530

    Googlead script

--- pages/blog/all.js
+++ pages/blog/all.js
@@ -8,6 +8,8 @@ export default function All(){
             <Head>
                 <meta name='theme-color' color='black'/>
                 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"/>
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3756551500003661"
+                 crossorigin="anonymous"></script>
             </Head>
             <h1 style={{color:'white',fontSize:55,fontWeight:'bold',marginTop:55,marginLeft:10}}>Blog</h1>
             <BlogCardComponent/>
