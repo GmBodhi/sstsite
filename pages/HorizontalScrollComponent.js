51022624b3bf660815ca5f95fd75aceb28d91441
sreedhar <sreedharks2003@gmail.com>
2022-07-24 12:57:44 +0530

    extension fix

--- pages/HorizontalScrollComponent.js
+++ pages/HorizontalScrollComponent.js
@@ -1,6 +1,6 @@
 import styles from '../styles/HorizontalScrollComponent.module.css';
 
-const contents=['https://i.ibb.co/mStgMbZ/Whats-App-Image-2022-07-24-at-12-51-46-PM.jpg','https://i.ibb.co/W6RWGch/Whats-App-Image-2022-07-24-at-12-51-48-PM.jp']
+const contents=['https://i.ibb.co/mStgMbZ/Whats-App-Image-2022-07-24-at-12-51-46-PM.jpg','https://i.ibb.co/W6RWGch/Whats-App-Image-2022-07-24-at-12-51-48-PM.jpg']
 export default function HorizontalScrollComponent(){
     return(
         <div className={styles.Hcontainer}>
