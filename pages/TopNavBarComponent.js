96ce1ba8babb8a4e20410fc221ff6529be6cbd30
sreedhar <sreedharks2003@gmail.com>
2022-07-29 23:43:28 +0530

    New micro animatatuons

--- pages/TopNavBarComponent.js
+++ pages/TopNavBarComponent.js
@@ -13,9 +13,9 @@ export default function TopNavBarComponent(){
         <header className="header">
             <nav className='w3-top w3-bar nav'>
                 
-                <div className={router.asPath=='/' ? 'w3-text-white  w3-bar-item ' :'w3-text-grey  w3-bar-item'} style={{marginRight:15,fontSize:20}}><Link href="/">home</Link></div>
-                <div className={router.asPath=='/about' ? 'w3-text-white  w3-bar-item' :'w3-text-grey  w3-bar-item'}  style={{marginRight:15,fontSize:20}}><Link href="/about">about</Link></div>
-                <div className={router.pathname=='/blog/all' ? 'w3-text-white  w3-bar-item' :'w3-text-grey  w3-bar-item'} style={{marginRight:15,fontSize:20}}><Link href="/blog/all">blog</Link></div>
+                <div className={router.asPath=='/' ? 'w3-text-white  w3-bar-item navitem navitemAll' :'w3-text-grey  w3-bar-item navitemAll'} style={{marginRight:15,fontSize:20}}><Link href="/">home</Link></div>
+                <div className={router.asPath=='/about' ? 'w3-text-white  w3-bar-item navitem' :'w3-text-grey  w3-bar-item navitemAll'}  style={{marginRight:15,fontSize:20}}><Link href="/about">about</Link></div>
+                <div className={router.pathname=='/blog/all' ? 'w3-text-white  w3-bar-item navitem' :'w3-text-grey  w3-bar-item navitemAll'} style={{marginRight:15,fontSize:20}}><Link href="/blog/all">blog</Link></div>
                
             </nav>
         </header>
