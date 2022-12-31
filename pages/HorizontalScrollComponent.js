49617a893807242c1c0d81571eb49413df7d5dbb
sreedhar <sreedharks2003@gmail.com>
2022-12-31 13:17:22 +0530

    changed HorizontalView contents and css

--- pages/HorizontalScrollComponent.js
+++ pages/HorizontalScrollComponent.js
@@ -1,18 +1,31 @@
 import styles from '../styles/HorizontalScrollComponent.module.css';
-
-const contents=['https://i.ibb.co/mStgMbZ/Whats-App-Image-2022-07-24-at-12-51-46-PM.jpg','https://i.ibb.co/W6RWGch/Whats-App-Image-2022-07-24-at-12-51-48-PM.jpg','https://i.ibb.co/tQZm3Br/Whats-App-Image-2022-07-24-at-1-04-01-PM.jpg']
+import globalstyles from '../styles/Home.module.css';
+const contents=[
+    {id:1,url:'https://dhyuthimetaverse.vercel.app',media:'https://i.ibb.co/4sLMfmn/dhyuthi.jpg'},
+    {id:2,url:'https://internshipcell.vercel.app',media:'https://i.ibb.co/hMfjgVG/internship-cell.jpg'},
+    {id:3,url:'https://github.com/mellofordev/potatoapi',media:'https://i.ibb.co/fFvGqVW/punfuel-project.jpg'},
+    {id:4,url:'https://github.com/mellofordev/swag-',media:'https://i.ibb.co/KVtd3Qg/swag.jpg'}
+]
 export default function HorizontalScrollComponent(){
     return(
+    <>
+       <h1 className={globalstyles.titleFont}>Some projects</h1>
         <div className={styles.Hcontainer}>
+            
             {contents.map((i)=>{
                 return(
-                    <img 
-                        className={styles.card}
-                        key={i}
-                        src={i}
-                    />
+                    <>
+                      <a href={i.url} target={'_blank'}>
+                        <img 
+                            className={styles.card}
+                            key={i.id}
+                            src={i.media}
+                        />
+                    </a>
+                    </>
                 )
             })}
         </div>
+    </>
     );
 }
