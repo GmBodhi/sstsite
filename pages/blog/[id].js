aded56352a4b7ff2c4cf234885c213cfcf3cca97
sreedhar <sreedharks2003@gmail.com>
2023-04-02 13:19:21 +0530

    Fixed the css of blog component

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import styles from '../../styles/Blog.module.css'
 export default function Blog(){
     const get_id = useRouter();
     let _id=get_id.query.id;
@@ -40,14 +41,16 @@ export default function Blog(){
 
     return(
         <div className="main">
-            {loading==true? <p style={{fontFamily:'Enhanced LED Board-7',fontSize:45,color:'white'}}>loading...</p>:(
-            <div key={Data.id} style={{width:'100%'}} className="blogContentView">
+            {loading==true? <p className={styles.loading}>loading...</p>:(
+            <div key={Data.id} style={{width:'100%'}} className={styles.blogContainer}>
                 <button  style={{color:'lightblue',backgroundColor:'black',fontSize:19,borderColor:'black',borderWidth:1}} onClick={()=>{get_id.back()}}>close</button>
-                <h1 style={{color:'white',fontSize:55,marginLeft:25}}>{Data.title}</h1>
-                <div key={Data.id}>
-                  <img style={{margin:24,borderRadius:15,width:'90%',height:'100%'}} src={`https://sreedbackend.pythonanywhere.com/${Data.pic}`}/>
+                <div className={styles.flexContainer}>
+                    <h1 style={{color:'white',fontSize:55,marginLeft:25}}>{Data.title}</h1>
+                    <div key={Data.id}>
+                    <img className={styles.blogImage} src={`https://sreedbackend.pythonanywhere.com/${Data.pic}`}/>
+                    </div>
+                    <p className={styles.content} >{Data.content}</p>
                 </div>
-                <p style={{color:'white',fontSize:25,margin:24,textAlign:'left',fontStretch:'expanded'}} >{Data.content}</p>
             </div>
             )}
             
