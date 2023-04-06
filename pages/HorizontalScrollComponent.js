b297620fdc9eb5688b990aec31db22d5f8236860
sreedhar <sreedharks2003@gmail.com>
2023-04-06 11:20:01 +0530

    added new project

--- pages/HorizontalScrollComponent.js
+++ pages/HorizontalScrollComponent.js
@@ -1,10 +1,11 @@
 import styles from '../styles/HorizontalScrollComponent.module.css';
 import globalstyles from '../styles/Home.module.css';
 const contents=[
-    {id:1,url:'https://dhyuthimetaverse.vercel.app',media:'https://i.ibb.co/4sLMfmn/dhyuthi.jpg'},
-    {id:2,url:'https://internshipcell.vercel.app',media:'https://i.ibb.co/hMfjgVG/internship-cell.jpg'},
-    {id:3,url:'https://github.com/mellofordev/potatoapi',media:'https://i.ibb.co/fFvGqVW/punfuel-project.jpg'},
-    {id:4,url:'https://github.com/mellofordev/swag-',media:'https://i.ibb.co/KVtd3Qg/swag.jpg'}
+    {id:2,url:'https://gdscwow.deta.dev',media:'https://i.ibb.co/6nr62Nk/gdscwow.png'},
+    {id:2,url:'https://dhyuthimetaverse.vercel.app',media:'https://i.ibb.co/4sLMfmn/dhyuthi.jpg'},
+    {id:3,url:'https://internshipcell.vercel.app',media:'https://i.ibb.co/hMfjgVG/internship-cell.jpg'},
+    {id:4,url:'https://github.com/mellofordev/potatoapi',media:'https://i.ibb.co/fFvGqVW/punfuel-project.jpg'},
+    {id:5,url:'https://github.com/mellofordev/swag-',media:'https://i.ibb.co/KVtd3Qg/swag.jpg'}
 ]
 export default function HorizontalScrollComponent(){
     return(
