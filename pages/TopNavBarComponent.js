eca0870bc4df12666a9cc03b3126da1d6175893f
sreedhar <sreedharks2003@gmail.com>
2024-02-04 16:19:20 +0530

    added new animations and styles for topnav

--- pages/TopNavBarComponent.js
+++ pages/TopNavBarComponent.js
@@ -12,12 +12,16 @@ export default function TopNavBarComponent(){
            <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"/>
         </Head>
         <header className="topnavheader">
-            <nav className='w3-top w3-bar topnav'>
+            <motion.nav className='w3-top w3-bar topnav'
+            initial={{opacity:0,transform:'translateY(-20px)'}}
+            whileInView={{opacity:1,transform:'translateY(0px)'}}
+            transition={{ ease:'linear', duration: 0.6}}
+            >
                 <div className={'w3-text-grey  w3-bar-item navitemAll navtext'}>
                     <img src='https://i.ibb.co/PMDzyY1/student-union.png' 
                     style={{height:50,width:50,filter: 'brightness(0) invert(1)'}}/>
                 </div>               
-            </nav>
+            </motion.nav>
         </header>
         </div>
     );
