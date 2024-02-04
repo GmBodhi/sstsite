b869b56d86cd6965b8d126edcf08702037427d26
sreedhar <sreedharks2003@gmail.com>
2024-02-04 17:25:21 +0530

    added college logo to topnav

--- pages/TopNavBarComponent.js
+++ pages/TopNavBarComponent.js
@@ -17,9 +17,12 @@ export default function TopNavBarComponent(){
             whileInView={{opacity:1,transform:'translateY(0px)'}}
             transition={{ ease:'linear', duration: 0.6}}
             >
-                <div className={'w3-text-grey  w3-bar-item navitemAll navtext'}>
+                <div className={'w3-text-grey  w3-bar-item navitemAll navtext'} style={{display:'flex',flexDirection:'row'}}>
                     <img src='https://i.ibb.co/PMDzyY1/student-union.png' 
                     style={{height:50,width:50,filter: 'brightness(0) invert(1)'}}/>
+                    <div style={{borderLeft:'1px solid  hsla(0,0%,100%,.155)',height:50,marginLeft:5,marginRight:5}}></div>
+                    <img src='https://i.ibb.co/GxLprT1/sctce-logo.png' 
+                    style={{height:50,width:150,filter: 'brightness(0) invert(1)',objectFit:'cover'}}/>
                 </div>               
             </motion.nav>
         </header>
