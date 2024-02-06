b9fa98bbb5292589740a46f803dc6621b142ccb2
Govardhan <govardhand06@gmail.com>
2024-02-06 18:39:24 +0530

    Changed toast to dialog 	modified:   components/BlogCardComponent.js 	new file:   components/DialogueComponent.js 	new file:   components/ui/dialog.jsx

--- components/BlogCardComponent.js
+++ components/BlogCardComponent.js
@@ -10,7 +10,11 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 
+import Dialogue from "./DialogueComponent";
+
 import { toast } from "sonner"
+import { Dialog } from "@radix-ui/react-dialog";
+
 
  
 export default function BlogCardComponent({option}){
@@ -74,6 +78,7 @@ export default function BlogCardComponent({option}){
         <div className='w3-row-padding'>
             
             {loading==true && <p style={{color:'white',textAlign:'center',fontSize:34,fontFamily:'Enhanced LED Board-7'}}>loading...</p>}
+            
             {Data.map((i)=>{
                 return(
                   <Card className="w-auto dark mb-5" key={i.id}>
@@ -87,21 +92,44 @@ export default function BlogCardComponent({option}){
                       </div>
                     </CardContent>
                     <CardFooter className="flex justify-between">
-                      <Button  disabled={(registerLoading==true || i.is_registered==true )? true : false} onClick={()=>{
-                        if(i.program_type=='g'){
-                          toast("Heads up!", {
-                            description: "Group events registeration not opened yet !",
-                            action: {
-                              label: "Close",
-                              onClick:()=>{console.log("close")}
-                            },
-                          })
-                        }else{
-                          setID(i.id);
-                          if(id!=0)
-                            register();
-                        }
-                      }}>{i.is_registered==true ? 'Registered':'Register'}</Button>
+                      <Dialog>
+                        <DialogTrigger>
+                        <Button  disabled={(registerLoading==true || i.is_registered==true )? true : false} onClick={()=>{
+                                            if(i.program_type=='g'){
+                                              toast("Heads up!", {
+                                                description: "Group events registeration not opened yet !",
+                                                action: {
+                                                  label: "Close",
+                                                  onClick:()=>{console.log("close")}
+                                                },
+                                              })
+                                            }else{
+                                              setID(i.id);
+                                              if(id!=0)
+                                      
+                                                register();
+                                            }
+                        }}>{i.is_registered==true ? 'Registered':'Register'}</Button>
+                        </DialogTrigger>
+                        <DialogContent>
+                            <DialogHeader>
+                            <DialogTitle>Are you absolutely sure?</DialogTitle>
+                            <DialogDescription>
+                                This action cannot be undone. This will permanently delete your account
+                                and remove your data from our servers.
+                            </DialogDescription>
+                            </DialogHeader>
+                            <DialogFooter className="sm:justify-start">
+                              <DialogClose asChild>
+                                <Button type="button" variant="secondary">
+                                  Register
+                                </Button>
+                              </DialogClose>
+                            </DialogFooter>
+
+                        </DialogContent>
+                      </Dialog>
+
                     </CardFooter>
                 </Card>
                 )
