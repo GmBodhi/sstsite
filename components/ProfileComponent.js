c94f39253d2b4eb22c568bba17dc9a65b9ae55ed
sreedhar <sreedharks2003@gmail.com>
2024-02-08 23:23:21 +0530

    fixed field typo in profile

--- components/ProfileComponent.js
+++ components/ProfileComponent.js
@@ -116,7 +116,7 @@ export default function ProfileComponent(){
                         <CardDescription className="text-1xl text-white">@{data.username}</CardDescription>
                         <CardDescription className="text-1xl text-white">Department: {data.department} </CardDescription>
                         <Button onClick={()=>{
-                            window.open(`whatsapp://send?phone=9633986935&text=Hi, my profile data is different  id : ${data.userame} name : ${data.name}`);
+                            window.open(`whatsapp://send?phone=9633986935&text=Hi, my profile data is different  id : ${data.username} name : ${data.name}`);
                         }}>Report Issue</Button>
                         
                         {data.department==='default' &&
